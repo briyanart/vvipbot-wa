b74d02190c051cf1d4e0c314a79545a301dc63c6
briyanart <79201424+briyanart@users.noreply.github.com>
2021-02-17 18:50:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'BriyanArt BOT'; 
+const instagram = 'briyanartb21'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Briyan\n' // Nama kamu
+            + 'ORG:BriyanArt;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6281314056521:+62 813-1405-6521\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
